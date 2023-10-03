d36b8c663ffebdb1396a3b494cbafe4026e045f1
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-10-03 23:21:41 +0200

    alx

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_node - insert a number
+ * @head: data type double
+ * @number: data type int
+ * Return: 0 or 1
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node = *head;
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+
+	if (node == NULL || node->n >= number)
+	{
+		new->next = node;
+		*head = new;
+		return (new);
+	}
+	while (node && node->next && node->next->n < number)
+		node = node->next;
+
+	new->next = node->next;
+	node->next = new;
+
+	return (new);
+}
