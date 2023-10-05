7c1d5a38e5015b1f76cb7d750a90a9317025e143
RetagFathiAs <retagfathi128@gmail.com>
2023-10-05 14:28:25 -0700

    alx

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
     import sys
 
     num = len(sys.argv) - 1
-
+    count = 0
     for i in range(num):
-        print("{}".format(int(i) + int(sys.argv[i + 1]) ))
+        count = count + int(sys.argv[i + 1])
+    print(f"{count}")
