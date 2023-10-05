f9f1b5cc3934c5850b3e9a6c27b9677a07c8535b
RetagFathiAs <retagfathi128@gmail.com>
2023-10-05 15:50:51 -0700

    alx

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,14 +4,16 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-    if len(sys.argv) < 4:
-      print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-      exit(1)
-
-    oplist = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[3] not in list(oplist.keys()):
+    blt_op = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(blt_op.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    print("{} {} {} = {}".format(sys.argv[2],sys.argv[3],sys.argv[4],oplist[sys.argv[3]](sys.argv[2],sys.argv[4])))
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    print("{} {} {} = {}".format(a, sys.argv[2], b, blt_op[sys.argv[2]](a, b)))
