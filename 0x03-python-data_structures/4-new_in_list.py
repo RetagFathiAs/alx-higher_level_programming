92f476539f048415c7caed09ada8df73300a2cf6
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-10-08 01:13:49 +0200

    alx

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-	sec_list.append(my_list)
+	sec_list = my_list.copy()
 	if idx < 0 or idx > (len(my_list) - 1):
 		return my_list
 	sec_list[idx] = element
