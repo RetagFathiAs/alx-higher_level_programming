6cddf1f3053187a5fe25fa50cc2beed0fb757f88
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-10-08 00:55:50 +0200

    alx

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    sec_list = []
-    len(sec_list) = len(my_list)
-    j = len(my_list)
-    for count in range(len(my_list)):
-        sec_list.append(my_list[j])
-        j = j - 1
-    for counts in range(len(my_list)-1):
-        print("{}".format(sec_list[counts]))
+    sec_list = my_list[::-1]
+    for item in sec_list:
+        print("{}".format(item))
