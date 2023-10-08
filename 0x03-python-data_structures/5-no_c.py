c04ca809e9ad32dd4c80f36a6977e37268cf2b79
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-10-08 19:29:52 +0200

    alx

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    i = 0
-    for i in range(len(my_string[])):
-        if my_string[i] != 'c' or my_string[i] != 'C':
-            return my_string[i]
+    sec_list = list(my_string)
+    index = 0
+    for i in sec_list:
+        if i == 'c' or == 'C':
+            sec_list[index] = ""
+        index += 1
+    return sec_list
