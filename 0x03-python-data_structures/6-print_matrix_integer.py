7addabed11507eddff079fda031417e98a405ad3
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-10-08 23:21:48 +0200

    alx

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        print()
+    for i in range(len(matrix)):
+      for j in range(len(matrix[i]):
+        if j != (len(matrix[i]) - 1)
+          space = " "
+        else:
+        space = ''
+      print("{:d}".format(matrix[i][j]), end = space)
+    print()
