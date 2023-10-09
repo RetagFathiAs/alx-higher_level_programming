a92d46708001454e012f822d0ae69ccc15badceb
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-10-09 23:39:15 +0200

    alx

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,32 +3,35 @@
 #include <stdlib.h>
 
 /**
- * is_palindrome - Check is the listint is a Palindrome
- * @head: type listint_s double pointer of node
+ * is_palindrome - Check
+ * @head: type listint_s double
  * return: 1 if is a pilndrome 0 if not
  */
 
 int is_palindrome(listint_t **head)
 {
-        if (head == NULL && *head == NULL)
-            return (1);
-        else
-            return (check_p(head, *head));
+	if (head == NULL && *head == NULL)
+		return (1);
+	else
+		return (rec_palindrome(head, *head));
 }
- /**
- * check_p - Recursion to check each node as palindrome
- * @head: type listint_s double pointer of node
- * @tail: type listint_s single pointer of last node
+
+/**
+ * rec_palindrome - Recursion to check
+ * @head: type listint_s double
+ * @tail: type listint_s single
  * return: 1 if is a pilndrome 0 if not
- */             
-int check_p(listint_t **head, listint_t *tail)
+ */
+
+int rec_palindrome(listint_t **head, listint_t *tail)
 {
-        if (tail == NULL)
-                return (1);
-        if (check_p(head, tail->next) && (*head)->n == tail->n)
-        {
-              *head = (*head)->next;
-              return (1);
-        }
-        return (0);
+	if (tail == NULL)
+		return (1);
+
+	if (rec_palindrome(head, tail->next) && (*head)->n == tail->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
 }
