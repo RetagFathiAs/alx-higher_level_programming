abb80ad80121b593ba1e46a67bbd512858e9ce7e
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-10-09 23:45:17 +0200

    alx

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -2,7 +2,6 @@
 #define LISTS_H
 
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -10,12 +9,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ * for project
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -23,6 +22,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-int rec_palindrome(listint_t **head, listint_t *tail);
+int check_p(listint_t **head, listint_t *tail);
 
 #endif /* LISTS_H */
