417fef95c61c640d8b82936a6f7aaac19ff1c12e
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-10-09 23:22:03 +0200

    allx

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <Python.h>
+
 /**
  * print_python_list_info - Print some basic info about Python lists
  * @p: PyObject
  *
  * Return: Nothing
  */
+
 void print_python_list_info(PyObject *p)
 {
 PyObject *item;
