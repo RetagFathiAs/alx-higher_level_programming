ed58414f0237b296262f721830312063e5c9afef
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-10-23 23:11:17 +0200

    alx

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Python.h>
+
+/**
+ * print_python_list - Function that prints basic info about python objects
+ * @p: PyObject
+ */
+
+void print_python_list(PyObject *p)
+{
+}
+
+/**
+ * print_python_bytes - Function that prints basic info about python objects
+ * @p: PyObject
+ */
+
+void print_python_bytes(PyObject *p)
+{
+}
+
+/**
+ * print_python_bytes - Function that prints basic info about python objects
+ * @p: PyObject
+ */
+
+void print_python_float(PyObject *p)
+{
+}
