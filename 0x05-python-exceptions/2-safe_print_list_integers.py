e729cee320c31459b00b485aefd33e15df436a72
RetagFathiAs <retagfathi128@gmail.com>
2023-10-23 14:19:45 -0700

    alx

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             r_element = r_element + 1
-        except:
+        except (ValueError, TypeError):
             continue
     print("")
     return (r_element)
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         d = a / b
-    except:
+    except (ZeroDivisionError, TypeError):
         d = None
     finally:
         print("Inside result: {}".format(d))
