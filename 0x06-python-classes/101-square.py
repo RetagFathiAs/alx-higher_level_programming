671a9204982cdb13d909a5781c85fdaa4639d351
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-10-24 23:20:41 +0200

    alx

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
 
-"""Define  Square."""
+"""Define a Square."""
 
 
 class Square:
-    """Represent a square."""
+    """square."""
 
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
         Args:
-            size (int): The size of t
-            position (int, int): The posite.
+            size (int): The size
+            position (int, int): The position
         """
         self.size = size
         self.position = position
 
-
     @property
     def size(self):
+        """Get/set the curr."""
         return (self.__size)
-
-    @property
-    def position(self):
-        return (self.__position)
 
     @size.setter
     def size(self, value):
@@ -32,19 +28,26 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        """Get/set the current position of the square."""
+        return (self.__position)
+
     @position.setter
     def position(self, value):
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
-                not all(isinstance(number, int) for number in value) or
-                not all(number >= 0 for number in value)):
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
+        """Return the current"""
         return (self.__size * self.__size)
 
     def my_print(self):
+        """Print the square"""
         if self.__size == 0:
             print("")
             return
@@ -56,8 +59,9 @@ class Square:
             print("")
 
     def __str__(self):
+        """Define the print() f a Square."""
         if self.__size != 0:
-            [print("")for i in range(0, self.__position[1])]
+            [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
