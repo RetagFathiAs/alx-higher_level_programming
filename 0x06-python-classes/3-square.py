96b38f388663ac61549f6020748e9f3b76f184ac
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-10-24 23:23:05 +0200

    alx

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python3
-""" forth task """"
+""" defines a square """
 
 
 class Square:
-    """square"""
+    """ square with pibute size """
 
     def __init__(self, size=0):
-        """Init the square classs
-        Args:
-        param1: size is the type int attribute to make it private
         """
-        if not isinstance(size, int):
-            raise TypeError("size mut be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        initializes square
+        Args:
+            size: size of side of square
+        """
+
+        if type(size) is int:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
+            raise TypeError('size must be an integer')
 
     def area(self):
-        """Return the area"""
-        return (self.__size * self.__size)
+        """
+        finds area of square
+        Returns:
+            the area
+        """
+
+        return self.__size ** 2
