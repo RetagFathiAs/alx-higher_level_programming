4b3d65acbef31e70a16009e0d007a226ca730fd1
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-10-24 23:29:02 +0200

    alx

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,21 +1,102 @@
 #!/usr/bin/python3
-"""Defines a class MagicClass"""
-import math
+"""Square"""
 
 
-class MagicClass:
-    """This represents a circle"""
-    def __init__(self, radius=0):
-        """Initializes the Magic Class"""
-        self.__radius = 0
-        if type(radius) is not int and type(radius) is not float:
-            raise TypeError('radius must be a number')
-        self.__radius = radius
+class Square:
+    """Represents a square
+    Attributes:
+        __size (int): size of a uare
+    """
+    def __init__(self, size=0):
+        """initializes the square
+        Args:
+            size (int): size of a side square
+        Returns:
+            None
+        """
+        self.size = size
 
     def area(self):
-        """Calculaes the area of the circle"""
-        return (self.__radius ** 2) * math.pi
+        """calculates the area
+        Returns:
+            The area square
+        """
+        return (self.__size) ** 2
 
-    def circumference(self):
-        """Calculates the circumference of the circle"""
-        return 2 * math.pi * self.__radius
+    @property
+    def size(self):
+        """getter of __size
+        Returns:
+            The size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter of __size
+        Args:
+            value (int): the size of a size of the square
+        Returns:
+            None
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+
+    def __lt__(self, other):
+        """Compare if square is less than another by area
+        Args:
+            other (Square): square to compare against
+        Returns:
+            True or False
+        """
+        return self.size < other.size
+
+    def __le__(self, other):
+        """Compare if square is less than or equal to another by area
+        Args:
+            other (Square): square to compare against
+        Returns:
+            True or False
+        """
+        return self.size <= other.size
+
+    def __eq__(self, other):
+        """Compare if square is equal to another by area
+        Args:
+            other (Square): square to compare against
+        Returns:
+            True or False
+        """
+        return self.size == other.size
+
+    def __ne__(self, other):
+        """Compare if square is not equal to another by area
+        Args:
+            other (Square): square to compare against
+        Returns:
+            True or False
+        """
+        return self.size != other.size
+
+    def __ge__(self, other):
+        """Compare if square is greater than area
+        Args:
+            other (Square): square to against
+        Returns:
+            True or False
+        """
+        return self.size >= other.size
+
+    def __gt__(self, other):
+        """Compare if square is greater area
+        Args:
+            other (Square): square to compare against
+        Returns:
+            True or False
+        """
+        return self.size > other.size
