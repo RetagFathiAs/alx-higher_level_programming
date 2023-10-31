287dc61a2090e05a5cfe5c6bed6b735b7b4a3a3c
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-10-31 20:40:35 +0200

    alx

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-""" python """
-
-
 def magic_string():
-    """ python """
-
     magic_string.ntimes = getattr(magic_string, 'ntimes', 0) + 1
-    return ("Holberton, " * (magic_string.ntimes - 1) + "Holberton")
+    return ("BestSchool, " * (magic_string.ntimes - 1) + "BestSchool")
