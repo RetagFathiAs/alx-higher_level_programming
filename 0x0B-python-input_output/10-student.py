0d9268f63b35d91f8b54aa2e375a3b835692c3d2
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-11-09 00:41:10 +0200

    alx

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-
+""" 
+yuyfthju 
+"""
 
 class Student:
-
+    """ hhhhhhhhhhhffffffffffff """
     def __init__(self, first_name, last_name, age):
-
+        """ jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-
+        """ jjjjjjjjjjjiiiiiiiiiiiiiijjjjjjjj"""
         if(type(attrs) == list and all(type(element) == str
                                        for element in attrs)):
             return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
