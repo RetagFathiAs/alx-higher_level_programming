399405cbc5e0a11efa20e58672f9ed5afaa4e704
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-11-09 00:28:12 +0200

    alx

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""json"""
+"""json fggggggggggggg"""
 
 
 import json
 
 
 def to_json_string(my_obj):
-    
     """ vbdhshfukyrjfsdnxcvnkjzeifils """
-    
     return json.dumps(my_obj)
