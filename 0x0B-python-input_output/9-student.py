339ceeb394f0fa6956089f3c0beeb9681ebc77a9
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-11-09 00:33:39 +0200

    alx

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,12 +3,14 @@
 
 
 class Student:
+    """ 9-student.py ffffffffffffffffile"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """ 9-student.py fghffffffffff"""
         dic1 = {}
         if hasattr(self, '__dict__'):
             return self.__dict__
